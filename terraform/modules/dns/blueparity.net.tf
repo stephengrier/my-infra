bbe69e34bc785b2b7dc52877008d4c8d03a3717f
Stephen Grier <git@grier.org.uk>
2019-11-08 01:21:19 +0000

    Remove unused records from blueparity.net zone

--- terraform/modules/dns/blueparity.net.tf
+++ terraform/modules/dns/blueparity.net.tf
@@ -28,22 +28,6 @@ resource "aws_route53_record" "blueparity_net_txt" {
   ]
 }
 
-resource "aws_route53_record" "blueparity_net" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "${local.blueparity_net_domain}"
-  type    = "A"
-  ttl     = "86400"
-  records = ["212.13.198.8"]
-}
-
-resource "aws_route53_record" "www_blueparity_net" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "www"
-  type    = "A"
-  ttl     = "86400"
-  records = ["212.13.198.8"]
-}
-
 resource "aws_route53_record" "mta1_blueparity_net" {
   zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
   name    = "mta1"
@@ -68,22 +52,6 @@ resource "aws_route53_record" "mta1_blueparity_net_txt" {
   records = ["v=spf1 a -all"]
 }
 
-resource "aws_route53_record" "mta1_domainkey_blueparity_net" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "mta1._domainkey"
-  type    = "TXT"
-  ttl     = "300"
-  records = ["v=DKIM1; t=y; k=rsa; p=MHwwDQYJKoZIhvcNAQEBBQADawAwaAJhAL+ePxQ2yh+cyM/2qKS2uB4//f2TKDQ4GSqN9ktKmXC6fACpPiDy1Cl9v4vC2Pdu393neILidmVk8OLGju5z+uf+Z31Snnf5nxfDVeQLegaa9Jl15voxDFtHTFTPzuqmDwIDAQAB;"]
-}
-
-resource "aws_route53_record" "domainkey_blueparity_net" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "_domainkey"
-  type    = "TXT"
-  ttl     = "300"
-  records = ["t=y; o=~;"]
-}
-
 resource "aws_route53_record" "mta2_blueparity_net" {
   zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
   name    = "mta2"
@@ -98,30 +66,6 @@ resource "aws_route53_record" "mta2_blueparity_net_txt" {
   type    = "TXT"
   ttl     = "86400"
   records = ["v=spf1 a -all"]
-}
-
-resource "aws_route53_record" "ns0_blueparity_net" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "ns0"
-  type    = "A"
-  ttl     = "86400"
-  records = ["212.13.198.10"]
-}
-
-resource "aws_route53_record" "ns0_blueparity_net_aaaa" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "ns0"
-  type    = "AAAA"
-  ttl     = "86400"
-  records = ["2001:ba8:0:1ce:2::53"]
-}
-
-resource "aws_route53_record" "ns1_blueparity_net" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "ns1"
-  type    = "A"
-  ttl     = "86400"
-  records = ["86.54.115.54"]
 }
 
 resource "aws_route53_record" "zebedee_blueparity_net" {
@@ -172,30 +116,6 @@ resource "aws_route53_record" "scooby_mgmt_blueparity_net" {
   records = ["192.168.214.3"]
 }
 
-resource "aws_route53_record" "console_blueparity_net" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "console"
-  type    = "A"
-  ttl     = "86400"
-  records = ["212.13.198.6"]
-}
-
-resource "aws_route53_record" "console_blueparity_net_aaaa" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "console"
-  type    = "AAAA"
-  ttl     = "86400"
-  records = ["2001:ba8:0:1ce:216:3eff:feff:fe01"]
-}
-
-resource "aws_route53_record" "console_mgmt_blueparity_net" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "console.mgmt"
-  type    = "A"
-  ttl     = "86400"
-  records = ["192.168.214.2"]
-}
-
 resource "aws_route53_record" "pugwash_blueparity_net" {
   zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
   name    = "pugwash"
@@ -218,12 +138,4 @@ resource "aws_route53_record" "imap_blueparity_net" {
   type    = "A"
   ttl     = "86400"
   records = ["54.217.215.155"]
-}
-
-resource "aws_route53_record" "vpn_blueparity_net" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "vpn"
-  type    = "A"
-  ttl     = "86400"
-  records = ["212.13.198.6"]
 }
