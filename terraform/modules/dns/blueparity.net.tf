ba107d2be38ba359e7427dbaf431eca8faf6a2ee
Stephen Grier <git@grier.org.uk>
2021-04-26 21:01:36 +0100

    Remove DNS records for zebedee server
    The zebedee server has been decommissioned so remove all DNS records for
    it and the scooby VM that ran on it.

--- terraform/modules/dns/blueparity.net.tf
+++ terraform/modules/dns/blueparity.net.tf
@@ -28,54 +28,6 @@ resource "aws_route53_record" "blueparity_net_txt" {
   ]
 }
 
-resource "aws_route53_record" "zebedee_blueparity_net" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "zebedee"
-  type    = "A"
-  ttl     = "86400"
-  records = ["212.13.198.5"]
-}
-
-resource "aws_route53_record" "zebedee_blueparity_net_aaaa" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "zebedee"
-  type    = "AAAA"
-  ttl     = "86400"
-  records = ["2001:ba8:0:1ce:21a:4dff:fef9:87c"]
-}
-
-resource "aws_route53_record" "zebedee_mgmt_blueparity_net" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "zebedee.mgmt"
-  type    = "A"
-  ttl     = "86400"
-  records = ["192.168.214.1"]
-}
-
-resource "aws_route53_record" "scooby_blueparity_net" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "scooby"
-  type    = "A"
-  ttl     = "86400"
-  records = ["212.13.198.7"]
-}
-
-resource "aws_route53_record" "scooby_blueparity_net_aaaa" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "scooby"
-  type    = "AAAA"
-  ttl     = "86400"
-  records = ["2001:ba8:0:1ce:5652:ff:fe3b:4867"]
-}
-
-resource "aws_route53_record" "scooby_mgmt_blueparity_net" {
-  zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
-  name    = "scooby.mgmt"
-  type    = "A"
-  ttl     = "86400"
-  records = ["192.168.214.3"]
-}
-
 resource "aws_route53_record" "pugwash_blueparity_net" {
   zone_id = "${aws_route53_zone.blueparity_net.zone_id}"
   name    = "pugwash"
