a3538b8883855dee8935a5add8687252a7ea8b7b
Stephen Grier <git@grier.org.uk>
2021-05-14 23:40:41 +0100

    Change MX record for blueparity.net to use Google's mail servers
    The blueparity.net domain has been transferred to google domains. Google
    domains provides an email forwarding feature, whereby it is possible to
    use Google's mail servers to receive and forward email onto a Gmail
    account.
    
    Change the MX records for the blueparity.net domain to use Google's mail
    servers.
    
    https://support.google.com/domains/answer/3251241
    https://support.google.com/domains/answer/9428703?hl=en-GB

--- terraform/modules/dns/blueparity.net.tf
+++ terraform/modules/dns/blueparity.net.tf
@@ -13,7 +13,11 @@ resource "aws_route53_record" "blueparity_net_mx" {
   ttl     = "86400"
 
   records = [
-    "10 mta3.blueparity.net.",
+    "5 gmr-smtp-in.l.google.com.",
+    "10 alt1.gmr-smtp-in.l.google.com.",
+    "20 alt2.gmr-smtp-in.l.google.com.",
+    "30 alt3.gmr-smtp-in.l.google.com.",
+    "40 alt4.gmr-smtp-in.l.google.com.",
   ]
 }
 
