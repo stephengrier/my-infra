bff895458dfdbb4412a537cad4f25e2a91c167f4
Stephen Grier <git@grier.org.uk>
2022-06-23 22:13:28 +0100

    Allow egress to port 443 from imap instance
    The imap instance provisioning process includes downloading the awscli
    zip file so needs to egress out to port 443. Add a imap_egress_https
    rule to the imap_instance security group to allow this.

--- terraform/modules/imap/security_groups.tf
+++ terraform/modules/imap/security_groups.tf
@@ -34,3 +34,12 @@ resource "aws_security_group_rule" "imap_egress_http" {
   to_port           = 80
   protocol          = "tcp"
 }
+
+resource "aws_security_group_rule" "imap_egress_https" {
+  security_group_id = "${aws_security_group.imap_instance.id}"
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+}
