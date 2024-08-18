e12cbf5fe793ef93f253478ed083e9e574fc227e
Stephen Grier <git@grier.org.uk>
2024-08-18 23:57:30 +0100

    Change MX records for grier.org.uk to squarespace
    The grier.org.uk domain has been moved from 123-reg.co.uk to
    Squarespace.com. Change the MX records to direct email for the domain
    through Squarespace's mail servers.
    
    Change the SPF record to include the SPF records for mailgun.org, which
    is the mail system used by Squarespace.
    
    Add a mx._domainkey record to make DKIM work correctly. The value for
    this was provided in the Squarespace console.

--- terraform/modules/dns/grier.org.uk.tf
+++ terraform/modules/dns/grier.org.uk.tf
@@ -13,8 +13,8 @@ resource "aws_route53_record" "grier_org_uk_mx" {
   ttl     = "3600"
 
   records = [
-    "10 smtp.secureserver.net.",
-    "20 mailstore1.secureserver.net.",
+    "10 mxa.mailgun.org.",
+    "10 mxb.mailgun.org.",
   ]
 }
 
@@ -22,9 +22,20 @@ resource "aws_route53_record" "grier_org_uk_txt" {
   zone_id = aws_route53_zone.grier_org_uk.zone_id
   name    = local.grier_org_uk_domain
   type    = "TXT"
-  ttl     = "86400"
+  ttl     = "3600"
 
   records = [
-    "v=spf1 include:secureserver.net ?all",
+    "v=spf1 include:mailgun.org ?all",
+  ]
+}
+
+resource "aws_route53_record" "mx_domainkey_grier_org_uk_txt" {
+  zone_id = aws_route53_zone.grier_org_uk.zone_id
+  name    = "mx._domainkey"
+  type    = "TXT"
+  ttl     = "3600"
+
+  records = [
+    "k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+qxN9FKwaiBKgaVZQMlap4EvFgquewi0Sae+MU6cwwAjVF8FRdMQ5qcuqfONDNnatsiY1aqEs++RPz/w7ed5vKXPkdYmqftOdnzH1jY/Z3DltWxccTiVuahNwmzlG4Q9rBc8/YI2TaX6vRjHWWe/R2e8QrMHgNj+8ji4TdfTx6QIDAQAB",
   ]
 }
